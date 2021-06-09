4d95a0991d5d7eb1c58060bccb3501d2c427ced6
Ed <brown@mun.ca>
2021-06-09 13:42:12 -0230

    Changes to crawler and 6 6b

--- crawler.js
+++ crawler.js
@@ -1,9 +1,10 @@
 
 // define the crawler component
-function crawler([width, height], capacity = 100) {
+function crawler([width, height], indexpoints = 20) {
 
     let inside_nodes = []
     let inside_g // empty selection? 
+    let indexlength = width / indexpoints // distance between ticks
     // let outside_g
     function crawl(selection) {
         let outside = selection.append("g")
@@ -37,14 +38,22 @@ function crawler([width, height], capacity = 100) {
         inside.transform = tf => outside.attr("transform", tf) // added to selection object
         crawl.reset() // depends on inside_g being set
         return inside
-    }
+    } 
 
     let tickcount = 0;
     function settrans(selection) {
-        selection.attr("transform", `translate(${width - tickcount * 10},0)`)
+        selection.attr("transform", `translate(${width - tickcount * indexlength},0)`)
     }
 
-    crawl.cycle = function (duration = 750) {
+    // bounds of the translated clipped region in its own co-ordinates
+    // nb this won;t be valid during transitions - almost always??? 
+    // sine we are transitioning to this region - same for all .
+    crawl.bounds = function(){
+        return [tickcount * indexlength - width, tickcount * indexlength ]
+    }
+    // can we get the actual transform value? during transitions?
+
+    crawl.cycle = function (duration = 1750) {
         function cycler() {
             tickcount++;
             d3.active(this)
@@ -60,6 +69,25 @@ function crawler([width, height], capacity = 100) {
     crawl.stop = function () {
         //d3.interrupt(inside_g.node()) // need to get interrupt working?
         inside_g.interrupt()
+        settrans(inside_g)
+    }
+
+    // advance to this point in a single transformation, for smooth single stepping
+    
+/** TODO: tick/index points should be index configurable to match the number
+    of points requested. this could speed up the cycling as well with fewer calls 
+*/
+    crawl.crawlto = function(index, duration = 1750) {
+        if(index < tickcount) return ; // can't reverse at the moment
+
+        let allduration = duration*Math.abs(index-tickcount) // this is a const per crawler
+        console.log(allduration)
+        tickcount = index
+
+        inside_g.transition()
+            .duration(allduration)
+            .ease(d3.easeLinear)
+            .call(settrans)
     }
 
     crawl.reset = function (clock = 0) {
